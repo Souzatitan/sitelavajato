ee5f857164357264f6eb456a151c7d6e51649346
Souzatitan <asouzanderson@gmail.com>
2026-05-06 22:51:03 -0300

    backend roduction url

--- src/services/agendaService.js
+++ src/services/agendaService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8000/api";
+const API = "https://sitelavajato-backend.onrender.com";
 
 export const getAgenda = async () => {
   const res = await axios.get(`${API}/horarios`);
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8000/api";
+const API = "https://sitelavajato-backend.onrender.com";
 
 // login cliente
 export const loginCliente = async (email, senha) => {
