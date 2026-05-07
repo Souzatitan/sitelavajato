bf11b6755f245e1055c2f1886bb60897c60f91a4
Souzatitan <asouzanderson@gmail.com>
2026-05-06 22:44:46 -0300

    backend production url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://sitelavajato-backend.onrender.com/",
 });
 
 // 🔐 adiciona o token automaticamente em TODAS requisições
